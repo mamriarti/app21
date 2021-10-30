1779bf0cd17a37b4128d7a94b169c3ddda1b2ac3
Alexandra <mamriarti@gmail.com>
2021-10-30 21:45:48 +0300

    +CategoryFactory, PostFactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,59 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-
+       $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
-
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
-
-        $work = Category::create([
-            'name' => 'Wokk',
-            'slug' => 'work'
-
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id'=>$family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet',
-            'body' =>'Lorem ipsum dolar sit amet.Lorem ipsum dolar sit amet.Lorem ipsum dolar sit amet',
-
-        ]);
-
-          Post::create([
-            'user_id' => $user->id,
-            'category_id'=>$personal->id,
-            'title' => 'My Personal Post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet',
-            'body' =>'Lorem ipsum dolar sit amet.Lorem ipsum dolar sit amet.Lorem ipsum dolar sit amet',
-
-        ]);
-
-          Post::create([
-            'user_id' => $user->id,
-            'category_id'=>$work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem ipsum dolar sit amet',
-            'body' =>'Lorem ipsum dolar sit amet.Lorem ipsum dolar sit amet.Lorem ipsum dolar sit amet',
-
-        ]);
+        
 
 
     }
