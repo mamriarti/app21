55c6ff2d9de759825683af229529a3056c34f45c
Alexandra <mamriarti@gmail.com>
2021-11-03 22:17:45 +0300

    Blade Components and CSS Grids in index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,47 +1,13 @@
 <x-layout>
+    @include ('posts._header')
 
-	@include('posts._header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts" />
 
- 
-
-        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-
-                <x-post-featured-card />
-
-            <div class="lg:grid lg:grid-cols-2">
-               
-                 <x-post-card  />
-                 <x-post-card  />
-
-            </div>
-
-            <div class="lg:grid lg:grid-cols-3">
-
-
-                <x-post-card/>
-                <x-post-card/>
-                <x-post-card/>
-
-
-            </div>
-        </main>
-
-
-<!-- @foreach ($posts as $post)
-	<article>
-		<h1>
-			<a href="/post/{{ $post->slug }}">
-			{{ $post->title}}
-			</a>
-		</h1>
-		<p> 
-			By <a href="/author/{{ $post->author->username }}">{{ $post->author->name}}</a> in <a href="/category/{{ $post->category->slug}}">{{ $post->category->name }} </a>
-		</p>
-		<div>
-			{{ $post->excerpt}}
-		</div>
-		
-	</article>
-   @endforeach -->
-
+           
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
+    </main>
 </x-layout>
