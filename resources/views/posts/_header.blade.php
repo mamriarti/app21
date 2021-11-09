989bf7e38456533b99919578ea28cd6568d9a6cc
Alexandra <mamriarti@gmail.com>
2021-11-09 22:19:13 +0300

    menu dropdown categories

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -3,13 +3,8 @@
                 Latest <span class="text-blue-500">Laravel From Scratch</span> News
             </h1>
 
-            <h2 class="inline-flex mt-2">By Lary Laracore <img src="/images/lary-head.svg"
-                                                               alt="Head of Lary the mascot"></h2>
 
-            <p class="text-sm mt-14">
-                Another year. Another update. We're refreshing the popular Laravel series with new content.
-                I'm going to keep you guys up to speed with what's going on!
-            </p>
+            
 
             <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
 
@@ -17,11 +12,12 @@
 
                 <div class="relative flex lg:inline-flex  bg-gray-100 rounded-xl">
 
-                    <div x-data="{ show: false }" @click="show = false">
-                        <button 
-                        @click="show =! show" 
-                        class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex inline-flex"
-                        >
+                    <x-dropdown>
+
+                     <x-slot name="trigger">
+                        
+
+                        <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex inline-flex">
                            {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
                        
@@ -37,9 +33,11 @@
                         </g>
                     </svg>
                      </button>
-                
+                </x-slot>
 
-                     <div x-show="show" class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display: none">
+                    <a href="/"
+                    class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
+                     All</a>
 
                         @foreach($categories as $category)
                         <a href="/category/{{$category->slug}}" class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
@@ -47,7 +45,7 @@
                           </a>
                          @endforeach
 
-
+                     </x-dropdown>
 
                     
 
@@ -81,7 +79,7 @@
 
                 <!-- Search -->
                 <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-                    
+
                     <form method="GET" action="#">
                         <input type="text" name="search" placeholder="Find something"
                                class="bg-transparent placeholder-black font-semibold text-sm">
