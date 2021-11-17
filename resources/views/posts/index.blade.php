c9b0c6e9ccb3aae48829aee6984a033fbb6a4e87
Alexandra <mamriarti@gmail.com>
2021-11-17 21:54:11 +0300

    create pagination simpleTailwind

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,6 +5,9 @@
         @if ($posts->count())
             <x-posts-grid :posts="$posts" />
 
+
+            {{ $posts->links() }}
+
            
         @else
             <p class="text-center">No posts yet. Please check back later.</p>
