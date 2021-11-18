6d8ca322dfa39a394225803b68bff4d41eb11d2e
Alexandra <mamriarti@gmail.com>
2021-11-18 22:14:18 +0300

    add form on page registred

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,12 +1,74 @@
 <x-layout>
 
 	<section class="px-6 py-8">
-    
-
-		<h1>Страница регистрации</h1>
 
 
-    
+
+    <main class="max-w-lg mx-auto mt-10 bg-gray-100  border border-gray-200 p-6 rounded-xl">
+
+        <h1 class="text-center font-bold text-xl"> Регистрация</h1>
+
+        <form method="POST" action="/register" class="mt-10">
+
+            <div class="mb-6">
+
+                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                       for="name">Name
+                </label>
+                <input class="border border-gray-400 p-2 w-full"
+                       type="text"
+                       name="name"
+                       id="name"
+                       required
+                >
+
+            </div>
+
+            <div class="mb-6">
+                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                       for="username">Username
+                </label>
+                <input class="border border-gray-400 p-2 w-full"
+                       type="text"
+                       name="username"
+                       id="username"
+                       required
+                >
+
+
+                @error('username')
+                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="mb-6">
+
+                <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                       for="name">Password
+                </label>
+                <input class="border border-gray-400 p-2 w-full"
+                       type="password"
+                       name="password"
+                       id="password"
+                       required
+                >
+
+            </div>
+
+            <div class="mb-6">
+                <button type="submit"
+                        class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-400">
+                    Submit
+                </button>
+            </div>
+
+        </form>
+
+    </main>
+
+
+
+
 
 
 
