80d859a7f25986429e6027609a2ee94db0036c6e
Alexandra <mamriarti@gmail.com>
2021-11-20 21:43:29 +0300

    add validation unique email and username

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -92,6 +92,18 @@
                 </button>
             </div>
 
+
+            @if($errors->any())
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li class="text-red-500 text-xs">
+                                {{ $error }}
+                            </li>
+                        @endforeach
+
+                    </ul>
+            @endif
+
         </form>
 
     </main>
