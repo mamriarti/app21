524d747c6faeb3f9f782ebd1b106599d59e8dbfb
Alexandra <mamriarti@gmail.com>
2021-11-23 21:53:24 +0300

    add page login

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Validation\ValidationException;
 
 use Illuminate\Http\Request;
 
@@ -12,6 +13,24 @@ class SessionsController extends Controller
        return view('sessions.create');
 
    }
+
+   public function store(){
+
+       $attributes = request()->validate([
+           'email' => 'required|email',
+           'password' => 'required',
+       ]);
+       if(auth()->attempt($attributes)){
+           session()->regenerate();
+           return redirect('/')->with('success', 'Добро пожаловать на сайт');
+
+       }
+       throw ValidationException::withMessages([
+           'email' => 'Не удалось проверить предоставленные вами учетные данные.'
+       ]);
+
+   }
+
 
     public function destroy()
     {
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -8,6 +8,70 @@
 
             <h1 class="text-center font-bold text-xl"> Вход на сайт</h1>
 
+            <form method="POST" action="/login" class="mt-10">
+                @csrf
+
+
+
+
+
+                <div class="mb-6">
+
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="email">Email
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="email"
+                           name="email"
+                           id="email"
+                           value="{{ old('email') }}"
+                           required
+                    >
+
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+
+                </div>
+
+                <div class="mb-6">
+
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                           for="name">Password
+                    </label>
+                    <input class="border border-gray-400 p-2 w-full"
+                           type="password"
+                           name="password"
+                           id="password"
+                           required
+                    >
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
+
+                </div>
+
+                <div class="mb-6">
+                    <button type="submit"
+                            class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-400">
+                        Войти
+                    </button>
+                </div>
+
+
+                @if($errors->any())
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li class="text-red-500 text-xs">
+                                {{ $error }}
+                            </li>
+                        @endforeach
+
+                    </ul>
+                @endif
+
+            </form>
+
 
 
         </main>
