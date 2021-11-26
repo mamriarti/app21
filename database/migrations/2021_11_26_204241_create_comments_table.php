e3f4165e91b305215d06e1f6d238115f32a6c592
Alexandra <mamriarti@gmail.com>
2021-11-26 21:19:35 +0300

    constrained()->cascadeOnDelete()

--- database/migrations/2021_11_26_204241_create_comments_table.php
+++ database/migrations/2021_11_26_204241_create_comments_table.php
@@ -15,10 +15,12 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('post_id');
-            $table->unsignedBigInteger('user_id');
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
+
+
         });
     }
 
