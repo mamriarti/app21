d678bf2c740d2f6249c2f205b3b5e5edc8d49df5
Alexandra <mamriarti@gmail.com>
2021-11-26 20:49:33 +0300

    add tb comment

--- database/migrations/2021_11_26_204241_create_comments_table.php
+++ database/migrations/2021_11_26_204241_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('body');
             $table->timestamps();
         });
     }
