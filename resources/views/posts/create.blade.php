9e015c6165421364d886bd9ec6c9c2feca6608e8
Alexandra <mamriarti@gmail.com>
2021-12-10 22:08:13 +0300

    Create the Publish Post Form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,76 @@
 <x-layout>
     <section class="px-6 py-8">
+        <x-panel class="max-w-sm mx-auto">
 
-        Хелло
+                <form method="POST" action="/admin/post">
+                    @csrf
+
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="title">
+                          Title
+                        </label>
+
+                        <input class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="title"
+                        id="title"
+                        required>
+
+                        @error('title')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                        @enderror
+
+                    </div>
+
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="title">
+                            Excerpt
+                        </label>
+
+                        <textarea class="border border-gray-400 p-2 w-full"
+                               name="excerpt"
+                               id="excerpt"
+                                  required></textarea>
+
+                        @error('excerpt')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                        @enderror
+
+                    </div>
+
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="title">
+                            Body
+                        </label>
+
+                        <textarea class="border border-gray-400 p-2 w-full"
+                               name="body"
+                               id="body"
+                                  required></textarea>
+
+
+                        @error('body')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                        @enderror
+
+                    </div>
+
+                    <x-submit-button>
+                        Publish
+                    </x-submit-button>
+
+
+                </form>
+        </x-panel>
     </section>
 
 
