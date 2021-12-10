806db19ba7ba4ffd4ae944e1ea42cec696639958
Alexandra <mamriarti@gmail.com>
2021-12-11 00:24:53 +0300

    add validation and create post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
     <section class="px-6 py-8">
         <x-panel class="max-w-sm mx-auto">
 
-                <form method="POST" action="/admin/post">
+                <form method="POST" action="/admin/posts">
                     @csrf
 
                     <div class="mb-6">
@@ -18,6 +18,25 @@
                         required>
 
                         @error('title')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                        @enderror
+
+                    </div>
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="slug">
+                            Slug
+                        </label>
+
+                        <input class="border border-gray-400 p-2 w-full"
+                               type="text"
+                               name="slug"
+                               id="slug"
+                               required>
+
+                        @error('slug')
                         <p class="text-red-500 text-xs mt-2">
                             {{ $message }}
                         </p>
@@ -57,6 +76,39 @@
 
 
                         @error('body')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                        @enderror
+
+                    </div>
+
+
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="category_id">
+                            Category
+                        </label>
+
+                        <select name="category_id"
+                            id="category_id">
+
+                                @php
+                                    $categories = \App\Models\Category::all();
+                                @endphp
+
+                                @foreach($categories as $category)
+
+                                    <option value="{{ $category->id }}">
+                                        {{ ucwords($category->name) }}
+                                    </option>
+
+                                @endforeach
+                        </select>
+
+
+
+                        @error('category_id')
                         <p class="text-red-500 text-xs mt-2">
                             {{ $message }}
                         </p>
