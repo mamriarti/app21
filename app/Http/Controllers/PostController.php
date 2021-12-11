c0107be74da8a6e27e221496d4f3d39214ce4554
Alexandra <mamriarti@gmail.com>
2021-12-11 23:31:51 +0300

    add form file validate and store Post Thumbnail

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -36,6 +36,7 @@ class PostController extends Controller
     {
         $attributes = request()->validate([
             'title' => 'required',
+            'thumbnail' => 'required|image',
             'slug' => ['required', Rule::unique('posts', 'slug')],
             'excerpt' => 'required',
             'body' => 'required',
@@ -43,6 +44,7 @@ class PostController extends Controller
         ]);
 
         $attributes['user_id'] = auth()->id();
+        $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnail');
         Post::create($attributes);
         return redirect('/');
     }
