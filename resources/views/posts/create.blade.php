cf5980dae88ec117dec110a63afa98184ba6c6d2
Alexandra <mamriarti@gmail.com>
2021-12-11 15:00:32 +0300

    add thumbnail file fot form post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,8 +1,11 @@
 <x-layout>
-    <section class="px-6 py-8">
-        <x-panel class="max-w-sm mx-auto">
+    <section class="px-6 max-w-md mx-auto">
+        <h1 class="text-lg font-bold mb-4">
+            Опубликовать новый Пост
+        </h1>
+        <x-panel>
 
-                <form method="POST" action="/admin/posts">
+                <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                     @csrf
 
                     <div class="mb-6">
@@ -36,6 +39,26 @@
                                name="slug"
                                id="slug"
                                value="{{ old('slug') }}"
+                               required>
+
+                        @error('slug')
+                        <p class="text-red-500 text-xs mt-2">
+                            {{ $message }}
+                        </p>
+                        @enderror
+
+                    </div>
+
+                    <div class="mb-6">
+                        <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                               for="thumbnail">
+                            Thumbnail
+                        </label>
+
+                        <input class="border border-gray-400 p-2 w-full"
+                               type="file"
+                               name="thumbnail"
+                               id="thumbnail"
                                required>
 
                         @error('slug')
