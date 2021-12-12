77773f18a7089b2ace83afc61edd4d6b4d77da9c
Alexandra <mamriarti@gmail.com>
2021-12-12 19:01:11 +0300

    add dinamic thumbnails

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -4,7 +4,15 @@
     {{ $attributes->merge(['class' => 'transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl']) }}>
     <div class="py-6 px-5 h-full flex flex-col">
         <div>
-            <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
+            @if($post->thumbnail == null)
+                <img src="/images/illustration-1.png"
+                     alt="{{ $post->title }}"
+                     class="rounded-xl">
+            @else
+                <img src="{{ asset('/storage/' . $post->thumbnail )}}"
+                     alt="{{ $post->title }}"
+                     class="rounded-xl">
+            @endif
         </div>
 
         <div class="mt-6 flex flex-col justify-between flex-1">
