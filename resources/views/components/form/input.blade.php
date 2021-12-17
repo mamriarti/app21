4c0c73ecbb0bc153361478ee8be553b0bad598fd
Alexandra <mamriarti@gmail.com>
2021-12-17 23:06:06 +0300

    create extend the Admin Layout

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,13 +1,13 @@
-@props(['name', 'fieldname' => '', 'type' => 'text', ])
+@props(['name', 'fieldname' => ''])
 
  <x-form.field>
        <x-form.label name="{{ $name }}" fieldname="{{ $fieldname }}" />
 
         <input class="border border-gray-200 p-2 w-full rounded"
-               type="{{ $type }}"
                name="{{ $name }}"
                id="{{ $name }}"
                value="{{ old($name) }}"
+               {{ $attributes }}
         >
 
      <x-form.error name="{{ $name }}"/>
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -4,76 +4,44 @@
 
 
 
-        <main class="max-w-lg mx-auto mt-10 bg-gray-100  border border-gray-200 p-6 rounded-xl">
+        <main class="max-w-lg mx-auto mt-10">
 
-            <h1 class="text-center font-bold text-xl"> Вход на сайт</h1>
-
-            <form method="POST" action="/login" class="mt-10">
-                @csrf
+            <x-panel>
 
 
 
+                    <h1 class="text-center font-bold text-xl"> Вход на сайт</h1>
 
-
-                <div class="mb-6">
-
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="email">Email
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="email"
-                           name="email"
-                           id="email"
-                           value="{{ old('email') }}"
-                           required
-                    >
-
-                    @error('email')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-
-                </div>
-
-                <div class="mb-6">
-
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="name">Password
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="password"
-                           name="password"
-                           id="password"
-                           required
-                    >
-                    @error('password')
-                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
-                    @enderror
-
-                </div>
-
-                <div class="mb-6">
-                    <button type="submit"
-                            class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-400">
-                        Войти
-                    </button>
-                </div>
-
-
-                @if($errors->any())
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li class="text-red-500 text-xs">
-                                {{ $error }}
-                            </li>
-                        @endforeach
-
-                    </ul>
-                @endif
-
-            </form>
+                    <form method="POST" action="/login" class="mt-10">
+                        @csrf
 
 
 
+
+                        <x-form.input name="email" type="email" autocomplete="username" fieldname="Введите Емейл"  />
+                        <x-form.input name="password" type="password" autocomplete="new-password" fieldname="Введите свой пароль" />
+
+
+                        <x-form.button>
+                            Log In
+                        </x-form.button>
+
+
+                        @if($errors->any())
+                            <ul>
+                                @foreach($errors->all() as $error)
+                                    <li class="text-red-500 text-xs">
+                                        {{ $error }}
+                                    </li>
+                                @endforeach
+
+                            </ul>
+                        @endif
+
+                    </form>
+
+
+            </x-panel>
         </main>
 
 
